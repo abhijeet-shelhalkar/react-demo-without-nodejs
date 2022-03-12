385bceb16ff38ca496fe71e57d091d7bfb58e2cb
abhijeet.shelhalkar <abhijeet.shelhalkar@hurix.com>
2022-03-12 11:48:04 +0530

    first commit to react demo without nodejs

---
+++ app.js
@@ -0,0 +1,35 @@
+// Obtain the root 
+const rootElement = document.getElementById('root');
+
+// Create a ES6 class component    
+class ShoppingList extends React.Component {
+  // Use the render function to return JSX component      
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>{this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Facebook</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// Create a function to wrap up your component
+function App() {
+  return (
+    <div>
+      <ShoppingList name="Social Media Platforms List:" />
+    </div>
+  );
+}
+
+// Use the ReactDOM.render to show your component on the browser
+ReactDOM.render(<App />, rootElement);
+
+
+
+/* chrome.exe --disable-web-security --user-data-dir="D:/ChromeDisable" */
